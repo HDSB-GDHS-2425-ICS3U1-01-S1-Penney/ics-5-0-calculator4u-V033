e7a63d680ec5dfc78125995c0bac645e586fb0d1
V033 <1gordonmia@hdsb.ca>
2024-09-13 12:38:09 -0400

    Not finished, just saving
    Started on the addition part of the calculator.
    
    Current assignment status: A concerning amount of trial and error

---
+++ index.js
@@ -0,0 +1,26 @@
+
+console.log("Hello! Welcome to The Simple Calculator :D");
+
+console.log("Please type your equation into the box when prompted");
+
+let usersFirstValue = prompt("Please type your first value here");
+
+let usersSecondValue = prompt("Please type your second value here");
+
+let x = usersFirstValue;
+let y = usersSecondValue;
+
+function additionOfTwoValues(x, y){
+    const result = x + y;
+    console.log(result);
+}
+
+additionOfTwoValues(x, y);
+
+
+13
+
+
+
+
+
