1560dc3d62074b612a01e39a97045940a65e49a8
V033 <1gordonmia@hdsb.ca>
2024-09-16 11:33:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ console.log("Hello! Welcome to The Simple Calculator :D");
 
 console.log("Please type your equation into the box when prompted");
 
-let usersFirstValue = prompt("Please type your first value here");
+var usersFirstValue = prompt("Please type your first value here");
 
-let usersSecondValue = prompt("Please type your second value here");
+var usersSecondValue = prompt("Please type your second value here");
 
-let x = usersFirstValue;
-let y = usersSecondValue;
 
-function additionOfTwoValues(x, y){
-    const result = x + y;
+
+function additionOfTwoValues(num1, num2){
+    const result = num1 + num2;
     console.log(result);
 }
 
-additionOfTwoValues(x, y);
+additionOfTwoValues (userFirstValue, userSecondValue);
 
 
-13
+
+
 
 
 
