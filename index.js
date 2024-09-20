4eb5af22582e676935c7f0c5ef9c93fedeeb6156
V033 <1gordonmia@hdsb.ca>
2024-09-20 09:41:51 -0400

    Assignment Completed
    Made a calculator that adds, subtracts, multiplys, and divides. It also can detect if the user didn't input a correct operator or divided by 0, which will give them error messages.

--- index.js
+++ index.js
@@ -1,20 +1,66 @@
 
+// This will welcome users to the calculator and tell them what it can do
+
 console.log("Hello! Welcome to The Simple Calculator :D");
 
-console.log("Please type your equation into the box when prompted");
+console.log("With this calculator you'll be able to add, subtract, multiply, and divide!");
 
-var usersFirstValue = prompt("Please type your first value here");
+console.log("The following opperations are represented by the following symbols: Add = +, subtract = -, multiply= *, divide = /");
 
-var usersSecondValue = prompt("Please type your second value here");
+//This should (hopefully) get the users numbers
 
-
-
-function additionOfTwoValues(num1, num2){
-    const result = num1 + num2;
-    console.log(result);
+function getNumbers(){
+    const num1 = parseFloat(prompt("Please enter your first number"));
+    const num2 = parseFloat(prompt("Please enter your second number"));
+    return [num1, num2];
 }
 
-additionOfTwoValues (userFirstValue, userSecondValue);
+//The operations the calculator will be preforming happen here. I also added handeling invalid inputs for some of the additional features :)
+
+function calculate(num1, num2, operator) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 === 0) {
+                return 'An error has occured! Error: Division by zero';
+            } else {
+                result = num1 / num2;
+            }
+            break;
+        default:
+            return 'Invalid opperator :(';
+        }
+        return result; 
+
+    }
+
+    function workCalculator () {
+
+        const operator = prompt("What opperation would you like to do?");
+        const [num1, num2] = getNumbers();
+
+        const result = calculate(num1, num2, operator);
+        console.log("The answer is: ");
+        console.log(result);
+    }
+
+    //Actually starts the calculator
+    workCalculator();
+
+
+
+
+    
+
 
 
 
